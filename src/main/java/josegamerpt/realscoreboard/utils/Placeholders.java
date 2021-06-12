9bc8d9df725dbf03acab4cf33e5bb65c777d481d
José Rodrigues <josegamerpt@gmail.com>
2021-06-12 16:21:28 +0100

    + Updated Scoreboard library and Ping Method.

--- src/main/java/josegamerpt/realscoreboard/utils/Placeholders.java
+++ src/main/java/josegamerpt/realscoreboard/utils/Placeholders.java
@@ -1,6 +1,7 @@
 package josegamerpt.realscoreboard.utils;
 
 import josegamerpt.realscoreboard.RealScoreboard;
+import josegamerpt.realscoreboard.config.Config;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
@@ -18,12 +19,22 @@ public class Placeholders {
     private static Class<?> CPClass;
 
     public static int ping(Player p) {
+        String v = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        if (!p.getClass().getName().equals("org.bukkit.craftbukkit." + v + ".entity.CraftPlayer")) { //compatibility with some plugins
+            p = Bukkit.getPlayer(p.getUniqueId()); //cast to org.bukkit.entity.Player
+        }
         try {
-            Object entityPlayer = p.getClass().getMethod("getHandle").invoke(p);
-            return (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException e) {
-            e.printStackTrace();
-            return -1;
+            Class<?> CraftPlayerClass = Class.forName("org.bukkit.craftbukkit." + v + ".entity.CraftPlayer");
+            Object CraftPlayer = CraftPlayerClass.cast(p);
+            Method getHandle = CraftPlayer.getClass().getMethod("getHandle");
+            Object EntityPlayer = getHandle.invoke(CraftPlayer);
+            Field ping = EntityPlayer.getClass().getDeclaredField("ping");
+            return ping.getInt(EntityPlayer);
+        } catch (Exception e) {
+            if (Config.file().getBoolean("Debug")) {
+                e.printStackTrace();
+            }
+            return 0;
         }
     }
 
