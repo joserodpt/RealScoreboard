0b79d5320db76f23fa826af7aca34bb53359b5ef
Haha007 <nichtechtanonym@gmail.com>
2024-09-27 22:00:13 +0200

    Fix non persisted state

--- realscoreboard-api/src/main/java/joserodpt/realscoreboard/api/scoreboard/RSBPlayer.java
+++ realscoreboard-api/src/main/java/joserodpt/realscoreboard/api/scoreboard/RSBPlayer.java
@@ -95,7 +95,7 @@ public class RSBPlayer {
 
         if (sb != null) {
             this.current = sb;
-            if (!isScoreboardActive()) {
+            if (!isScoreboardActive() && isRealScoreboardVisible()) {
                 startScoreboard();
             }
         } else {
@@ -120,6 +120,7 @@ public class RSBPlayer {
             }
         }
         this.realScoreboardVisible = realScoreboardVisible;
+        RealScoreboardAPI.getInstance().getLogger().info("RealScoreboard visibility for " + p.getName() + " set to " + realScoreboardVisible);
         PlayerData playerData = RealScoreboardAPI.getInstance().getDatabaseManagerAPI().getPlayerData(p.getUniqueId());
         playerData.setScoreboardON(this.realScoreboardVisible);
         RealScoreboardAPI.getInstance().getDatabaseManagerAPI().savePlayerData(playerData, true);
